4bd9f5adb4c8dbfe248967b4e3f801e45a9d061e
FarzanehSadegh <farzanehsadegh@gmail.com>
2023-06-10 15:25:57 -0700

    transfer 4

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,19 @@ import {useState} from 'react';
 import Modal from 'react-modal';
 import './App.scss';
 // import Score from './Score';
-// import MainEasy from './MainEasy';
+import MainEasy from './MainEasy';
 // import ResultEasy from './ResultEasy';
 import RulesEasy from './RulesEasy';
 
-// import { ReactComponent as Lizard } from './images/icon-lizard.svg';
-// import { ReactComponent as Spock } from './images/icon-spock.svg';
 // import { ReactComponent as Close } from './images/icon-close.svg';
 // import { ReactComponent as RulesBonus } from './images/image-rules-bonus.svg';
 // import { ReactComponent as Rules } from './images/image-rules.svg';
-// import { ReactComponent as Pentagon } from './images/bg-pentagon.svg';
+
 
 function App() {
 
   // const [score, setScore] = useState(0);
-  // const [user, setUser] = useState(null);
+  const [user, setUser] = useState(null);
   const [openR, setOpenR] = useState(false);
 
   const closeModal = () => {
@@ -37,6 +35,7 @@ function App() {
       </Modal>
 
       {/* <Score score={score}/> */}
+      <MainEasy user={user} setUser={setUser}/> 
       {/* {!user ?
       <MainEasy user={user} setUser={setUser}/> :
       <ResultEasy user={user} setUser={setUser} setScore={setScore}/>
---
+++ src/MainEasy.jsx
@@ -0,0 +1,44 @@
+import './MainEasy.scss';
+import { ReactComponent as Paper } from './images/icon-paper.svg';
+import { ReactComponent as Rock } from './images/icon-rock.svg';
+import { ReactComponent as Scissors } from './images/icon-scissors.svg';
+import { ReactComponent as Triangle } from './images/bg-triangle.svg';
+
+function MainEasy({user, setUser}) {
+
+  const handleClickPaper = () => {
+    setUser('p');
+  } 
+  const handleClickScissors = () => {
+    setUser('s');
+  } 
+  const handleClickRock = () => {
+    setUser('r');
+  } 
+
+  return (
+    <div className="easy-main">
+      <div className='back'>
+
+        <Triangle className='triangle' />
+        <div className="paper-container">
+          <div className='key-paper' onClick={() => handleClickPaper()}>
+            <Paper/>
+          </div>
+        </div>
+        <div className="scissors-container">
+          <div className='key-scissors' onClick={() => handleClickScissors()}>
+            <Scissors/>
+          </div>
+        </div>
+        <div className="rock-container">
+          <div className='key-rock' onClick={() => handleClickRock()}>
+            <Rock />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default MainEasy;
